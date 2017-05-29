e4200b035346a9268bcd6b54e8c27a732e4853e2
angstsmurf <ignalina@me.com>
2017-05-29 16:12:04 +0200

    Some more non-functional hyperlink code.

--- glkimp/hyperlinks.c
+++ glkimp/hyperlinks.c
@@ -4,22 +4,60 @@
 
 void glk_set_hyperlink(glui32 linkval)
 {
-    //gli_strict_warning("set_hyperlink: hyperlinks not supported.");
+    gli_set_hyperlink(gli_currentstr, linkval);
 }
 
 void glk_set_hyperlink_stream(strid_t str, glui32 linkval)
 {
-    //gli_strict_warning("set_hyperlink_stream: hyperlinks not supported.");
+    if (!str)
+    {
+        gli_strict_warning("set_hyperlink_stream: invalid ref");
+        return;
+    }
+    gli_set_hyperlink(str, linkval);
+
 }
 
 void glk_request_hyperlink_event(winid_t win)
 {
-    //gli_strict_warning("request_hyperlink_event: hyperlinks not supported.");
+    if (!win)
+    {
+        gli_strict_warning("request_hyperlink_event: invalid ref");
+        return;
+    }
+
+    switch (win->type)
+    {
+        case wintype_TextBuffer:
+        case wintype_TextGrid:
+        case wintype_Graphics:
+            win->hyper_request = TRUE;
+            break;
+        default:
+            /* do nothing */
+            break;
+    }
 }
 
 void glk_cancel_hyperlink_event(winid_t win)
 {
-    //gli_strict_warning("cancel_hyperlink_event: hyperlinks not supported.");
+    if (!win)
+    {
+        gli_strict_warning("cancel_hyperlink_event: invalid ref");
+        return;
+    }
+
+    switch (win->type)
+    {
+        case wintype_TextBuffer:
+        case wintype_TextGrid:
+        case wintype_Graphics:
+            win->hyper_request = FALSE;
+            break;
+        default:
+            /* do nothing */
+            break;
+    }
 }
 
 #endif /* GLK_MODULE_HYPERLINKS */
--- glkimp/stream.c
+++ glkimp/stream.c
@@ -1686,3 +1686,80 @@ glui32 glk_get_buffer_stream(stream_t *str, char *buf, glui32 len)
     return gli_get_buffer(str, buf, NULL, len);
 }
 
+#ifdef GLK_MODULE_HYPERLINKS
+
+static void gli_set_hyperlink(stream_t *str, glui32 linkval)
+{
+    if (!str || !str->writable)
+        return;
+
+    switch (str->type)
+    {
+        case strtype_Window:
+            str->win->attr.hyper = linkval;
+            break;
+    }
+    
+}
+
+void glk_set_hyperlink(glui32 linkval)
+{
+    gli_set_hyperlink(gli_currentstr, linkval);
+}
+
+void glk_set_hyperlink_stream(strid_t str, glui32 linkval)
+{
+    if (!str)
+    {
+        gli_strict_warning("set_hyperlink_stream: invalid ref");
+        return;
+    }
+    
+    gli_set_hyperlink(str, linkval);
+    
+}
+
+void glk_request_hyperlink_event(winid_t win)
+{
+    if (!win)
+    {
+        gli_strict_warning("request_hyperlink_event: invalid ref");
+        return;
+    }
+
+    switch (win->type)
+    {
+        case wintype_TextBuffer:
+        case wintype_TextGrid:
+        case wintype_Graphics:
+            win->hyper_request = TRUE;
+            break;
+        default:
+            /* do nothing */
+            break;
+    }
+}
+
+void glk_cancel_hyperlink_event(winid_t win)
+{
+    if (!win)
+    {
+        gli_strict_warning("cancel_hyperlink_event: invalid ref");
+        return;
+    }
+
+    switch (win->type)
+    {
+        case wintype_TextBuffer:
+        case wintype_TextGrid:
+        case wintype_Graphics:
+            win->hyper_request = FALSE;
+            break;
+        default:
+            /* do nothing */
+            break;
+    }
+}
+
+#endif /* GLK_MODULE_HYPERLINKS */
+
