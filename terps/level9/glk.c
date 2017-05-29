1cf590348acca77883599111ad269cf1ba8078c4
angstsmurf <ignalina@me.com>
2017-05-29 14:03:40 +0200

    Include some of the Gargoyle specific modifications for the Level 9 interpreter.

--- terps/level9/glk.c
+++ terps/level9/glk.c
@@ -1709,7 +1709,7 @@ gln_graphics_clear_and_border (winid_t glk_window,
    */
   glk_window_set_background_color (glk_window, background);
   glk_window_clear (glk_window);
-#ifndef GARGLK
+#if !(defined(GARGLK) || defined(SPATTERLIGHT))
   /*
    * For very small pictures, just border them, but don't try and
    * do any shading.  Failing this check is probably highly unlikely.
@@ -3649,7 +3649,7 @@ gln_status_print (void)
         {
           int index;
 
-#ifndef GARGLK
+#if !(defined(GARGLK) || defined(SPATTERLIGHT))
           /* Set fixed width font to try to preserve status line formatting. */
           glk_set_style (style_Preformatted);
 #endif
@@ -5287,7 +5287,7 @@ gln_expand_abbreviations (char *buffer, int size)
       memmove (command + strlen (expansion) - 1, command, strlen (command) + 1);
       memcpy (command, expansion, strlen (expansion));
 
-#ifndef GARGLK
+#if !(defined(GARGLK) || defined(SPATTERLIGHT))
       gln_standout_string ("[");
       gln_standout_char (abbreviation);
       gln_standout_string (" -> ");
