108cc8c3973704f4ce41c5ef21236dec793d37a6
angstsmurf <ignalina@me.com>
2018-09-18 18:07:32 +0200

    Comment unused assignment to silence warning

--- terps/glulxe/operand.c
+++ terps/glulxe/operand.c
@@ -435,7 +435,7 @@ void parse_operands(oparg_t *args, operandlist_t *oplist)
         break;
 
       default:
-        value = 0;
+        /* value = 0; */
         fatal_error("Unknown addressing mode in load operand.");
       }
 
