412c1ed77ad5372c1f1e63d359f43bcf257154ce
Petter Sjölund <ignalina@me.com>
2018-11-05 01:45:08 +0100

    Another attempt at fixing license window

--- application/HelpPanelController.m
+++ application/HelpPanelController.m
@@ -8,7 +8,6 @@
 
 #import "HelpPanelController.h"
 
-
 @implementation HelpPanelController
 
 - (IBAction)copyButton:(id)sender {
@@ -18,7 +17,6 @@
 
 	// Selecting all the text is pretty useless, but better than no feedback at all I guess
 	[_textView setSelectedRange:NSMakeRange(0, _textView.string.length)];
-
 }
 
 - (void) showHelpFile:(NSAttributedString *)text withTitle:(NSString *)title
@@ -33,35 +31,34 @@
 		CGRect screenframe = [[NSScreen mainScreen] visibleFrame];
 		CGRect contentrect = ((NSView *)helpWindow.contentView).frame;
 
-		CGFloat oldheight = helpWindow.frame.size.height;
-
 		NSString *string = text.string;
 
 		CGFloat textWidth = [self longestLine:string];
 
-		CGFloat windowWidth = textWidth + 44; //+ 42;
-		// 2 * 1 point border, 20 * 2 standard spacing between scroll view and window border
-
-		[_textView setTextContainerInset:NSMakeSize(10, 0)];
-
-
-		[helpWindow.contentView setFrameSize: NSMakeSize(windowWidth, contentrect.size.height)];
-
-		contentrect.size.width = windowWidth;
-
-		NSSize size = _textView.textContainer.containerSize;
-		size.width = textWidth; // + 2 * _textView.textContainerInset.width;
-
-		[_textView setFrameSize:size];
-        [_scrollView setFrameSize:size];
+		CGFloat oldheight = helpWindow.frame.size.height;
 
 		_textView.string = string;
 
-		float width = _textView.frame.size.width - 2 * _textView.textContainerInset.width;
+        NSSize containersize = _textView.textContainer.containerSize;
 
-		float proposedHeight = [self heightForString:_textView.string font:_textView.font andWidth:width
+        if (textWidth < screenframe.size.width)
+        {
+            containersize.width = textWidth;
+            [_textView.textContainer setContainerSize:containersize];
+        }
+        else
+        {
+            textWidth = containersize.width;
+        }
+
+        CGFloat windowWidth = textWidth + 19;
+
+        [helpWindow.contentView setFrameSize: NSMakeSize(windowWidth, contentrect.size.height)];
+
+		float proposedHeight = [self heightForString:_textView.string font:_textView.font andWidth:textWidth
 										  andPadding:_textView.textContainer.lineFragmentPadding];
 
+        contentrect = ((NSView *)helpWindow.contentView).frame;
 		contentrect.size.height = proposedHeight + 82; // 1 x standard 20pt margin above, 3 x below, 1pt border x 2
 
 		//Hopefully, by using frameRectForContentRect, this code will still work
@@ -72,15 +69,20 @@
 
 		//If the entire text does not fit on screen, don't change height at all
 		if (winrect.size.height > screenframe.size.height)
-			winrect.size.height = screenframe.size.height = oldheight;
+			winrect.size.height = oldheight;
 
 		// When we reuse the window it will remember our last scroll position,
 		// so we reset it here
-		[_scrollView.documentView scrollPoint:NSMakePoint(0.0, 0.0)];
-		//[_textView scrollRectToVisible: NSMakeRect(0, 0, 0, 0)];
+
+        // Scroll the vertical scroller to top
+        if ([_scrollView hasVerticalScroller]) {
+            _scrollView.verticalScroller.floatValue = 0;
+        }
+
+        // Scroll the contentView to top
+        [_scrollView.contentView scrollToPoint:NSMakePoint(0, 0)];
 
 		[self showWindow: helpWindow];
-
 
 		CGFloat offset = winrect.size.height - oldheight;
 
@@ -96,15 +98,12 @@
 		[helpWindow setFrame:winrect display:YES animate:YES];
 		[_textView scrollRectToVisible: NSMakeRect(0, 0, 0, 0)];
 	}
-
 	[helpWindow makeKeyAndOrderFront:nil];
 }
 
 - (CGFloat)longestLine:(NSString *)string
 {
-	CGRect screenframe = [[NSScreen mainScreen] visibleFrame];
-
-	NSString *longestLine;
+	NSString *proposedLine;
 
 	NSUInteger index, stringLength = [string length];
 	CGFloat textWidth = 0;
@@ -115,8 +114,8 @@
 	{
 		range = [string lineRangeForRange:NSMakeRange(index, 0)];
 		index = NSMaxRange(range);
-		longestLine = [string substringWithRange:range];
-		CGSize stringSize = [longestLine sizeWithAttributes:@{NSFontAttributeName:standardFont}];
+		proposedLine = [string substringWithRange:range];
+		CGSize stringSize = [proposedLine sizeWithAttributes:@{NSFontAttributeName:standardFont}];
 		CGFloat width = stringSize.width;
 
 		if (width > textWidth)
@@ -128,12 +127,6 @@
 	// Unless we add 2, the last word on the longest line will break. Some kind of 1-point border on each side, I guess. It is not the line fragment padding, that is 5.
 
 	textWidth += 2;
-
-	//NSLog(@"_textView.textContainer.lineFragmentPadding = %f",_textView.textContainer.lineFragmentPadding);
-
-	if (textWidth > screenframe.size.width)
-		textWidth = screenframe.size.width / 2;
-
 	return textWidth;
 }
 
@@ -156,28 +149,24 @@
 						defaultFrame:(NSRect)newFrame
 {
 	CGFloat oldheight = window.frame.size.height;
-	CGFloat windowWidth = [self longestLine:_textView.string] + 42;
-
-	float width = _textView.frame.size.width - 2 * _textView.textContainerInset.width;
-
-	float proposedHeight = [self heightForString:_textView.string font:_textView.font andWidth:width
+	CGFloat windowWidth = [self longestLine:_textView.string];
+	float proposedHeight = [self heightForString:_textView.string font:_textView.font andWidth:windowWidth
 									  andPadding:_textView.textContainer.lineFragmentPadding];
 
 	CGRect dummyrect = ((NSView *)window.contentView).frame;
 	dummyrect.size.height = proposedHeight + 82; // 1 standard 20pt margin above, 3 below, 1pt border
 
 	CGRect winrect = [window frameRectForContentRect:dummyrect];
-
 	CGFloat newheight = winrect.size.height;
 
-	newFrame.size.width = windowWidth;
+	newFrame.size.width = windowWidth + 19;
 	newFrame.size.height = newheight;
-    
+
 	CGFloat offset = newFrame.size.height - oldheight;
-    
+
 	newFrame.origin.y = window.frame.origin.y - offset;
 	newFrame.origin.x = ([[NSScreen mainScreen] visibleFrame].size.width - windowWidth) / 2;
-    
+
 	return newFrame;
 };
 
