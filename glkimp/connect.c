d92e6a9a0a9b8cec3565ff117dd7dc42a09506ef
Petter Sjölund <ignalina@me.com>
2018-11-16 12:56:50 +0100

    Let through all valid key presses
    I hope this is the correct behavior.

--- glkimp/connect.c
+++ glkimp/connect.c
@@ -602,8 +602,12 @@ again:
             event->type = evtype_CharInput;
             event->win = gli_window_for_peer(wmsg.a1);
             event->val1 = wmsg.a2;
-            if (!event->win->char_request_uni && event->val1 > 0xFF)
-                event->val1 = '?';
+            if (!event->win->char_request_uni)
+            {
+                unsigned chr =  (unsigned)event->val1;
+                if (chr > 0xFF && !(chr <= keycode_Unknown && chr >= keycode_Func12))
+                    event->val1 = '?';
+            }
             event->win->char_request = FALSE;
             event->win->char_request_uni = FALSE;
             break;
