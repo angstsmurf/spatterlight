006e5071b91941eb9ef4e74b530317698fdc3d3d
Petter Sjölund <ignalina@me.com>
2019-02-11 01:34:07 +0100

    Revert "Clean up contextual menu code"
    This reverts commit f381d31ced4be546e4f6de7208b1ecd281562cd6.

--- application/LibController.m
+++ application/LibController.m
@@ -429,10 +429,22 @@ static BOOL save_plist(NSString *path, NSDictionary *plist)
         else return NO;
     }
 
+    if (action == @selector(delete:))
+        return count > 0;
+
+    if (action == @selector(deleteGame:))
+        return count > 0;
+
+    if (action == @selector(revealGameInFinder:))
+        return count > 0;
+
+    if (action == @selector(showGameInfo:))
+        return count > 0;
+
     if (action == @selector(playGame:))
         return count == 1;
 
-   return count > 0;
+    return YES;
 }
 
 #pragma mark Drag-n-drop destination handler
