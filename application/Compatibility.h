8597cd5cd0d20deeda394c2fae8f9d1f4194503d
Petter Sjölund <ignalina@me.com>
2020-03-12 18:20:55 +0100

    Make Compatibility.h work on 10.10

--- application/Compatibility.h
+++ application/Compatibility.h
@@ -8,12 +8,16 @@
 
 #ifndef NS_OPTIONS
 #define NS_OPTIONS(_type, _name)                                               \
-    enum _name : _type _name;                                                  \
-    enum _name : _type
+enum _name : _type _name;                                                  \
+enum _name : _type
+#endif
+
+#if !defined(NSAppKitVersion)
+typedef double NSAppKitVersion;
 #endif
 
 #if !defined(MAC_OS_X_VERSION_10_9) ||                                         \
-    MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_9
+MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_9
 
 typedef NSString *const NSAccessibilityNotificationUserInfoKey;
 
@@ -21,25 +25,31 @@ typedef enum NSAccessibilityPriorityLevel : NSInteger {
     NSAccessibilityPriorityHigh = 90
 } NSAccessibilityPriorityLevel;
 
-typedef double NSAppKitVersion;
 
 APPKIT_EXTERN void
 NSAccessibilityPostNotificationWithUserInfo(id element, NSString *notification,
                                             NSDictionary *userInfo)
-    NS_AVAILABLE_MAC(10_7);
+NS_AVAILABLE_MAC(10_7);
 APPKIT_EXTERN NSString *const
-    NSAccessibilityAnnouncementRequestedNotification NS_AVAILABLE_MAC(10_7);
+NSAccessibilityAnnouncementRequestedNotification NS_AVAILABLE_MAC(10_7);
 APPKIT_EXTERN NSString *const
-    NSAccessibilityAnnouncementKey NS_AVAILABLE_MAC(10_7);
+NSAccessibilityAnnouncementKey NS_AVAILABLE_MAC(10_7);
 APPKIT_EXTERN const
-    NSAppKitVersion NSAppKitVersionNumber10_8 NS_AVAILABLE_MAC(10_7);
+NSAppKitVersion NSAppKitVersionNumber10_8 NS_AVAILABLE_MAC(10_7);
 APPKIT_EXTERN const
-    NSAppKitVersion NSAppKitVersionNumber10_9 NS_AVAILABLE_MAC(10_7);
-APPKIT_EXTERN const
-    NSAppKitVersion NSAppKitVersionNumber10_12 NS_AVAILABLE_MAC(10_7);
+NSAppKitVersion NSAppKitVersionNumber10_9 NS_AVAILABLE_MAC(10_7);
 
 APPKIT_EXTERN NSAccessibilityNotificationUserInfoKey NSAccessibilityPriorityKey
-    NS_AVAILABLE_MAC(10_7);
+NS_AVAILABLE_MAC(10_7);
 
 #endif // MAC_OS_X_VERSION_10_9
+
+#if !defined(MAC_OS_X_VERSION_10_12) ||                                         \
+MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_12
+
+APPKIT_EXTERN const
+NSAppKitVersion NSAppKitVersionNumber10_12 NS_AVAILABLE_MAC(10_7);
+
+#endif // MAC_OS_X_VERSION_10_12
+
 #endif // Spatterlight_compatibility_h
