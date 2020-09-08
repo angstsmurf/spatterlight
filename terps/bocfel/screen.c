6b0010437d3810e8718f7e7917c1e3c0849186ff
angstsmurf <ignalina@me.com>
2020-09-08 23:00:05 +0200

    BOCFEL: Fix sound support
    This adds a callback function that makes looping and fading work properly.

--- terps/bocfel/screen.c
+++ terps/bocfel/screen.c
@@ -56,6 +56,7 @@
 #include "unicode.h"
 #include "util.h"
 #include "zterp.h"
+#include "sound.h"
 
 bool header_fixed_font;
 
@@ -1743,6 +1744,19 @@ static bool get_input(uint16_t timer, uint16_t routine, struct input *input)
         }
 
         break;
+
+       case evtype_SoundNotify:
+        {
+            struct window *saved3 = curwin;
+            end_of_sound();
+
+            /* It’s possible for an interrupt to switch windows; if it
+             * does, simply switch back.  This is the easiest way to deal
+             * with an undefined bit of the Z-machine.
+             */
+            if(curwin != saved3) set_current_window(saved3);
+            break;
+        }
 
       case evtype_CharInput:
         ZASSERT(input->type == INPUT_CHAR, "got unexpected evtype_CharInput");
--- terps/bocfel/sound.h
+++ terps/bocfel/sound.h
@@ -3,9 +3,11 @@
 
 #include <stdbool.h>
 
-void init_sound(bool);
+void init_sound(void);
 bool sound_loaded(void);
 
 void zsound_effect(void);
+
+void end_of_sound(void);
 
 #endif
