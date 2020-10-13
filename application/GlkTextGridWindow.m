angstsmurf/spatterlight

commit eee42a529048ca58ab6e584fc96e2e1c4169acf3

GlkTextGridWindow: Ignore out-of-range input requests
Instead of trying to move them in range.
This allows us to drop the Robot Finds Kitten hack.

application/GlkTextGridWindow.m
@@ -1254,17 +1254,10 @@
 
     terminator = 0;
 
-    line_request = YES;
-
-    // Robot Finds Kitten requests repeated line input in the upper right corner during animation.
-    // We just ignore it.
-    if (self.glkctl.robotFindsKitten) {
-        if (ypos == 0 && xpos > 10 && maxLength == 0)
-            return;
-    }
-
     if (xpos + maxLength > cols || maxLength == 0)
-        maxLength = cols - xpos;
+        return;
+
+    line_request = YES;
 
     maxInputLength = maxLength;
 

application/GlkController.h
@@ -126,7 +126,6 @@
 @property BOOL deadCities;
 @property BOOL bureaucracy;
 @property BOOL beyondZork;
-@property BOOL robotFindsKitten;
 @property BOOL kerkerkruip;
 
 @property NSInteger autosaveVersion;

application/GlkController.m
@@ -178,9 +178,6 @@ fprintf(stderr, "%s\n",                                                    \
     if ([_game.ifid isEqualToString:@"ZCODE-47-870915"] || [_game.ifid isEqualToString:@"ZCODE-49-870917"] || [_game.ifid isEqualToString:@"ZCODE-51-870923"] || [_game.ifid isEqualToString:@"ZCODE-57-871221"] || [_game.ifid isEqualToString:@"ZCODE-60-880610"])
         _beyondZork = YES;
 
-    if ([_game.ifid isEqualToString:@"ZCODE-6-031116-1D88"] || [_game.ifid isEqualToString:@"ZCODE-7-130320-4A18"])
-        _robotFindsKitten = YES;
-
     if ([_game.ifid isEqualToString:@"AC0DAF65-F40F-4A41-A4E4-50414F836E14"])
         _kerkerkruip = YES;
 

commit 034f5357de2738930b6f129f659dcd04ef3935b0

GlkTextGridView: Fix line input in grid view again
eee42a5 broke it.

application/GlkTextGridWindow.m
@@ -1254,8 +1254,12 @@
 
     terminator = 0;
 
-    if (xpos + maxLength > cols || maxLength == 0)
+    if (xpos > cols || maxLength == 0) {
         return;
+    }
+
+    if (xpos + maxLength > cols + 1)
+        maxLength = cols + 1 - xpos;
 
     line_request = YES;
 