ec59dbdfd6c3ae032071f2b7179c2137c40b5a07
angstsmurf <ignalina@me.com>
2021-02-28 03:38:01 +0100

    glkimp: Use non-negative constants for Z-colors
    As defined by Chris  Spiegel in Gargoyle

--- glkimp/glk.h
+++ glkimp/glk.h
@@ -462,10 +462,10 @@ extern void garglk_set_config(const char *name);
 extern void garglk_unput_string(char *str);
 extern void garglk_unput_string_uni(glui32 *str);
 
-#define zcolor_Transparent   (-4)
-#define zcolor_Cursor        (-3)
-#define zcolor_Current       (-2)
-#define zcolor_Default       (-1)
+#define zcolor_Transparent   ((glui32)0xfffffffc)
+#define zcolor_Cursor        ((glui32)0xfffffffd)
+#define zcolor_Current       ((glui32)0xfffffffe)
+#define zcolor_Default       ((glui32)0xffffffff)
 
 extern void garglk_set_zcolors(glui32 fg, glui32 bg);
 extern void garglk_set_zcolors_stream(strid_t str, glui32 fg, glui32 bg);
