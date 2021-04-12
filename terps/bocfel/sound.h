0d12089d429571fecd055c60399caff9b5bfcc24
angstsmurf <ignalina@me.com>
2021-04-12 02:09:48 +0200

    Prepare for upgrade to Bocfel 1.3.
    1.3 includes a complete update to the coding style, so the upgrade to
    1.3 is being done in two parts. This commit is nothing but the style
    change. The next commit will be the functional changes, so that anybody
    wanting to see the material changes in 1.3 can look at the next commit
    and not be distracted by all the style changes.

--- terps/bocfel/sound.h
+++ terps/bocfel/sound.h
@@ -5,16 +5,9 @@
 
 #include <stdbool.h>
 
-#include "library_state.h"
-
-void stash_library_sound_state(library_state_data *dat);
-void recover_library_sound_state(library_state_data *dat);
-
 void init_sound(void);
 bool sound_loaded(void);
 
 void zsound_effect(void);
-
-void end_of_sound(void);
 
 #endif
