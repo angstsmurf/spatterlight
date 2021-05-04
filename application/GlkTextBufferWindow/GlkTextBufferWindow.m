angstsmurf/spatterlight

commit fedbea79e06ace3aef84f8e65701445b9b7ed4d8

Add margin images as text attachments

application/GlkTextBufferWindow/GlkTextBufferWindow.m
@@ -251,7 +251,6 @@ fprintf(stderr, "%s\n",                                                    \
     panel.nameFieldStringValue = newName;
     NSTextView *localTextView = _textview;
     NSAttributedString *localTextStorage = textstorage;
-    MarginContainer *localTextContainer = container;
     [panel
      beginSheetModalForWindow:window
      completionHandler:^(NSInteger result) {
@@ -260,9 +259,6 @@ fprintf(stderr, "%s\n",                                                    \
 
             NSMutableAttributedString *mutattstr =
             [localTextStorage mutableCopy];
-
-            mutattstr = [localTextContainer
-                         marginsToAttachmentsInString:mutattstr];
 
             [mutattstr
              enumerateAttribute:NSBackgroundColorAttributeName
@@ -281,11 +277,9 @@ fprintf(stderr, "%s\n",                                                    \
             if (isRtfd) {
                 NSFileWrapper *wrapper;
                 wrapper = [mutattstr
-                           RTFDFileWrapperFromRange:NSMakeRange(
-                                                                0, mutattstr.length)
+                           RTFDFileWrapperFromRange:NSMakeRange(0, mutattstr.length)
                            documentAttributes:@{
-                               NSDocumentTypeDocumentAttribute :
-                                   NSRTFDTextDocumentType
+                               NSDocumentTypeDocumentAttribute:NSRTFDTextDocumentType
                            }];
 
                 [wrapper writeToURL:theFile
@@ -502,12 +496,14 @@ fprintf(stderr, "%s\n",                                                    \
     _pendingScrollRestore = YES;
     _pendingScroll = NO;
 
-    NSArray *marginimgscopy = [container.marginImages copy];
-
-    for (MarginImage *img in marginimgscopy) {
+    for (MarginImage *img in container.marginImages) {
         img.container = container;
         img.accessibilityParent = _textview;
         img.bounds = [img boundsWithLayout:layoutmanager];
+        NSTextAttachment *attachment = [textstorage attribute:NSAttachmentAttributeName atIndex:img.pos + 1 effectiveRange:nil];
+        MyAttachmentCell *cell = (MyAttachmentCell *)attachment.attachmentCell;
+        cell.marginImage = img;
+        cell.accessibilityLabel = cell.customA11yLabel;
     }
 
     if (!self.glkctl.inFullscreen || self.glkctl.startingInFullscreen)
@@ -585,8 +581,6 @@ fprintf(stderr, "%s\n",                                                    \
         [self storeScrollOffset];
     }
 
-    //    [self recalcInputAttributes];
-
     GlkController *glkctl = self.glkctl;
 
     // Adjust terminators for Beyond Zork arrow keys hack
@@ -638,7 +632,6 @@ fprintf(stderr, "%s\n",                                                    \
     // get lost when we update the Glk Styles.
 
     if (different) {
-        NSLog(@"Different");
         styles = newstyles;
         [self recalcInputAttributes];
 
@@ -1175,7 +1168,6 @@ fprintf(stderr, "%s\n",                                                    \
 }
 
 - (void)recalcInputAttributes {
-    NSLog(@"recalcInputAttributes");
     NSMutableDictionary *inputStyle = [styles[style_Input] mutableCopy];
     if (currentZColor && self.theme.doStyles && currentZColor.fg != zcolor_Current && currentZColor.fg != zcolor_Default) {
         inputStyle[NSForegroundColorAttributeName] = [NSColor colorFromInteger: currentZColor.fg];
@@ -1493,7 +1485,6 @@ replacementString:(id)repl {
     NSTextAttachment *att;
     NSFileWrapper *wrapper;
     NSData *tiffdata;
-    // NSAttributedString *attstr;
 
     [self flushDisplay];
 
@@ -1508,51 +1499,41 @@ replacementString:(id)repl {
     if (h == 0)
         h = (NSInteger)image.size.height;
 
+    image = [self scaleImage:image size:NSMakeSize(w, h)];
+
+    tiffdata = image.TIFFRepresentation;
+
+    wrapper = [[NSFileWrapper alloc] initRegularFileWithContents:tiffdata];
+    wrapper.preferredFilename = @"image.tiff";
+    att = [[NSTextAttachment alloc] initWithFileWrapper:wrapper];
+
+    MyAttachmentCell *cell =
+    [[MyAttachmentCell alloc] initImageCell:image
+                               andAlignment:align
+                                  andAttStr:textstorage
+                                         at:textstorage.length - 1];
+
     if (align == imagealign_MarginLeft || align == imagealign_MarginRight) {
         if (_lastchar != '\n' && textstorage.length) {
             NSLog(@"lastchar is not line break. Do not add margin image.");
             return;
-        }
-        //        NSLog(@"adding image to margins");
-        unichar uc[1];
-        uc[0] = NSAttachmentCharacter;
-
-        [textstorage.mutableString
-         appendString:[NSString stringWithCharacters:uc length:1]];
-
-        image = [self scaleImage:image size:NSMakeSize(w, h)];
-
-        [container addImage:image align: align at:
-         textstorage.length - 1 linkid:(NSUInteger)self.currentHyperlink];
-
-        if (self.currentHyperlink) {
-            [textstorage addAttribute:NSLinkAttributeName value:@(self.currentHyperlink) range:NSMakeRange(textstorage.length - 1, 1)];
-        }
-    } else {
-        //        NSLog(@"adding image to text");
-        image = [self scaleImage:image size:NSMakeSize(w, h)];
-
-        tiffdata = image.TIFFRepresentation;
-
-        wrapper = [[NSFileWrapper alloc] initRegularFileWithContents:tiffdata];
-        wrapper.preferredFilename = @"image.tiff";
-        att = [[NSTextAttachment alloc] initWithFileWrapper:wrapper];
-        MyAttachmentCell *cell =
-        [[MyAttachmentCell alloc] initImageCell:image
-                                   andAlignment:align
-                                      andAttStr:textstorage
-                                             at:textstorage.length - 1];
-        att.attachmentCell = cell;
-        NSMutableAttributedString *attstr =
-        (NSMutableAttributedString *)[NSMutableAttributedString
-                                      attributedStringWithAttachment:att];
-
-        [textstorage appendAttributedString:attstr];
-
-        if (self.currentHyperlink) {
-            [textstorage addAttribute:NSLinkAttributeName value:@(self.currentHyperlink) range:NSMakeRange(textstorage.length - 1, 1)];
+        } else {
+            [container addImage:image align: align at:
+             textstorage.length - 1 linkid:(NSUInteger)self.currentHyperlink];
+            cell.marginImage = container.marginImages.lastObject;
         }
     }
+
+    att.attachmentCell = cell;
+    NSAttributedString *attstr = [NSAttributedString
+                                  attributedStringWithAttachment:att];
+
+    [textstorage appendAttributedString:attstr];
+
+    if (self.currentHyperlink) {
+        [textstorage addAttribute:NSLinkAttributeName value:@(self.currentHyperlink) range:NSMakeRange(textstorage.length - 1, 1)];
+    }
+
     [textstorage addAttributes:styles[style] range:NSMakeRange(textstorage.length -1, 1)];
 }
 
@@ -1895,7 +1876,7 @@ replacementString:(id)repl {
 }
 
 - (void)scrollToBottom {
-    NSLog(@"GlkTextBufferWindow %ld scrollToBottom", self.name);
+//    NSLog(@"GlkTextBufferWindow %ld scrollToBottom", self.name);
     lastAtTop = NO;
     lastAtBottom = YES;
 
@@ -1987,17 +1968,15 @@ replacementString:(id)repl {
     NSAttributedString *attStr = [_textview accessibilityAttributedStringForRange:range];
     NSMutableString *string = attStr.string.mutableCopy;
 
-    // Look for image attachments (margin image descriptions are already added)
+    // Look for image attachments and add their descriptions
     NSArray *keys;
     NSDictionary *attachments = [self attachmentsInRange:range withKeys:&keys];
     NSUInteger offset = 0;
     for (NSNumber *num in keys) {
         NSUInteger index = (NSUInteger)num.intValue - range.location + offset;
         MyAttachmentCell *cell = (MyAttachmentCell *)((NSTextAttachment *)attachments[num]).attachmentCell;
-        NSString *label = cell.accessibilityLabel;
-        if (!label)
-            label = cell.accessibilityRoleDescription;
-        label = [NSString stringWithFormat:@"(Image: %@.)", label];
+        NSString *label = cell.customA11yLabel;
+        label = [NSString stringWithFormat:@"(%@)", label];
         [string insertString:label atIndex:index];
         offset += label.length;
     }
@@ -2129,38 +2108,26 @@ replacementString:(id)repl {
     return links;
 }
 
-- (NSArray *)images {
-    NSRange allText = NSMakeRange(0, textstorage.length);
-    if (self.moveRanges.count < 2)
-        return [self imagesInRange:allText];
-    NSMutableArray <NSValue *> *images = [[NSMutableArray alloc] init];
-
-    // Make sure that no text after last moveRange slips through
-    NSRange lastMoveRange = self.moveRanges.lastObject.rangeValue;
-    NSRange stubRange = NSMakeRange(NSMaxRange(lastMoveRange), textstorage.length);
-    stubRange = NSIntersectionRange(allText, stubRange);
-    if (stubRange.length) {
-        [images addObjectsFromArray:[self imagesInRange:stubRange]];
-    }
-
-    for (NSValue *rangeVal in self.moveRanges)
-    {
-        // Print some info
-        [images addObjectsFromArray:[self imagesInRange:rangeVal.rangeValue]];
-    }
-    if (images.count > 15)
-        images.array = [images subarrayWithRange:NSMakeRange(images.count - 15, 15)];
-
-    //    NSLog(@"images: found %ld images", images.count);
+- (NSArray<NSValue *> *)images {
+    NSArray<NSValue *> *images = [self imagesInRange:_textview.accessibilityVisibleCharacterRange];
     return images;
 }
 
-- (NSArray *)imagesInRange:(NSRange)range {
-    NSArray *keys;
-    NSDictionary *attachments = [self attachmentsInRange:range withKeys:&keys];
-    NSArray *images = [self addMarginImagesInRange:range toDictionary:attachments];
-    return images;
+- (NSArray<NSValue *> *)imagesInRange:(NSRange)range {
+   __block NSMutableArray<NSValue *> *images = [NSMutableArray new];
+   [textstorage
+    enumerateAttribute:NSAttachmentAttributeName
+    inRange:range
+    options:0
+    usingBlock:^(id value, NSRange subrange, BOOL *stop) {
+       if (!value) {
+           return;
+       }
+       [images addObject:[NSValue valueWithRange:subrange]];
+   }];
+   return images;
 }
+
 
 - (NSDictionary <NSNumber *, NSTextAttachment *> *)attachmentsInRange:(NSRange)range withKeys:(NSArray **)keys {
     __block NSMutableDictionary <NSNumber *, NSTextAttachment *> *attachments = [NSMutableDictionary new];
@@ -2185,32 +2152,32 @@ replacementString:(id)repl {
     *keys = mutKeys;
     return attachments;
 }
-
-- (NSArray *)addMarginImagesInRange:(NSRange)range toDictionary:(NSDictionary <NSNumber *, NSTextAttachment *> *)dictionary {
-    NSMutableDictionary *mutable = dictionary.mutableCopy;
-    for (MarginImage *img in container.marginImages) {
-        if (img.pos >= range.location && img.pos < NSMaxRange(range))
-            mutable[@(img.pos)] = img;
-    }
-
-    NSArray *keys = [mutable.allKeys sortedArrayUsingComparator:
-                     ^NSComparisonResult(id obj1, id obj2){
-        NSInteger key1 = ((NSNumber *)obj1).integerValue;
-        NSInteger key2 = ((NSNumber *)obj2).integerValue;
-        if (key1 > key2) {
-            return (NSComparisonResult)NSOrderedDescending;
-        }
-        if (key1 < key2) {
-            return (NSComparisonResult)NSOrderedAscending;
-        }
-        return (NSComparisonResult)NSOrderedSame;
-    }];
-
-    NSMutableArray *result = [NSMutableArray new];
-    for (NSNumber *key in keys)
-        [result addObject:mutable[key]];
-
-    return result;
-}
+//
+//- (NSArray *)addMarginImagesInRange:(NSRange)range toDictionary:(NSDictionary <NSNumber *, NSTextAttachment *> *)dictionary {
+//    NSMutableDictionary *mutable = dictionary.mutableCopy;
+//    for (MarginImage *img in container.marginImages) {
+//        if (img.pos >= range.location && img.pos < NSMaxRange(range))
+//            mutable[@(img.pos)] = img;
+//    }
+//
+//    NSArray *keys = [mutable.allKeys sortedArrayUsingComparator:
+//                     ^NSComparisonResult(id obj1, id obj2){
+//        NSInteger key1 = ((NSNumber *)obj1).integerValue;
+//        NSInteger key2 = ((NSNumber *)obj2).integerValue;
+//        if (key1 > key2) {
+//            return (NSComparisonResult)NSOrderedDescending;
+//        }
+//        if (key1 < key2) {
+//            return (NSComparisonResult)NSOrderedAscending;
+//        }
+//        return (NSComparisonResult)NSOrderedSame;
+//    }];
+//
+//    NSMutableArray *result = [NSMutableArray new];
+//    for (NSNumber *key in keys)
+//        [result addObject:mutable[key]];
+//
+//    return result;
+//}
 
 @end

application/GlkTextBufferWindow/MarginImage.m
@@ -22,8 +22,7 @@
 
 - (instancetype)init {
     return [self
-        initWithImage:[[NSImage alloc]
-                          initWithContentsOfFile:@"../Resources/Question.png"]
+        initWithImage:[[NSImage alloc] initWithContentsOfFile:@"../Resources/Question.png"]
                 align:kAlignLeft
                linkid:0
                    at:0
@@ -46,7 +45,6 @@
         _container = sender;
 
         self.accessibilityParent = _container.textView;
-
         self.accessibilityRoleDescription = [self customA11yLabel];
     }
     return self;
@@ -151,9 +149,10 @@
 - (NSString *)customA11yLabel {
     NSString *label = _image.accessibilityDescription;
     if (!label.length) {
-        label = [NSString stringWithFormat: @"%@ margin image", _alignment == imagealign_MarginLeft ? @"Left" : @"Right"];
         if (_linkid) {
-            label = [label stringByAppendingFormat:@" with link I.D. %ld", _linkid];
+            label = [NSString stringWithFormat: @"Clickable %@ margin image", _alignment == imagealign_MarginLeft ? @"left" : @"right"];
+        } else {
+            label = [NSString stringWithFormat: @"%@ margin image", _alignment == imagealign_MarginLeft ? @"Left" : @"Right"];
         }
     }
     return label;

application/GlkTextBufferWindow/BufferTextView.m
@@ -243,77 +243,6 @@
    return [((GlkTextBufferWindow *)self.delegate).glkctl createCustomRotors];
 }
 
-- (NSAttributedString *)accessibilityAttributedStringForRange:(NSRange)range {
-//    NSLog(@"accessibilityAttributedStringForRange %@", NSStringFromRange(range));
-    NSAttributedString *result = [super accessibilityAttributedStringForRange:range];
-    NSUInteger location = range.location;
-    NSArray *marginImages = ((MarginContainer *)self.textContainer).marginImages;
-    if (marginImages && marginImages.count) {
-        NSMutableArray <MarginImage *> *imagesInRange = marginImages.mutableCopy;
-        for (MarginImage *mi in marginImages) {
-            if (mi.pos < location || mi.pos >= NSMaxRange(range))
-                [imagesInRange removeObject:mi];
-        }
-        if (imagesInRange.count) {
-            NSString *string = result.string;
-            NSString *attachmentCharacter = @"\uFFFC";
-            NSArray <NSString *> *attachmentRanges = [string componentsSeparatedByString:attachmentCharacter];
-            if (attachmentRanges.count)
-            {
-                NSUInteger relativeLocation = 0;
-                NSUInteger index = 0;
-                NSMutableAttributedString *newResult = [NSMutableAttributedString new];
-//                NSLog(@"Range: %@ First attachment character at pos %ld, attachmentRanges.count:%ld", NSStringFromRange(range), posOfAtt, attachmentRanges.count);
-                for (NSString *substring in attachmentRanges) {
-//                    NSLog(@"Attachment %ld: %@ (%ld characters)", index, substring, substring.length);
-                    NSMutableAttributedString *attSubstring = [result attributedSubstringFromRange:NSMakeRange(relativeLocation, substring.length)].mutableCopy;
-//                    NSLog(@"Attachment found at pos %ld", location + relativeLocation + 1);
-                    relativeLocation += substring.length + 1;
-                    if (!attSubstring)
-                        NSLog(@"attSubstring nil!");
-                    if (!attSubstring.length)
-                        NSLog(@"attSubstring empty!");
-                    if (index < imagesInRange.count) {
-                        NSAttributedString *label = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"(Image: %@.) ", imagesInRange[index].customA11yLabel]];
-                        [attSubstring appendAttributedString:label];
-                        index++;
-                    }
-                    [newResult appendAttributedString: attSubstring];
-                }
-                result = newResult;
-            }
-        }
-    }
-    return result;
-}
-
-//- (void)setAccessibilitySelectedTextRange:(NSRange)range {
-//    NSLog(@"setAccessibilitySelectedTextRange: %@", NSStringFromRange(range));
-//    if (range.length < 2) {
-//        if (self.string.length > range.location && [self.string characterAtIndex:range.location] == NSAttachmentCharacter ) { //&& range.length < 2
-//            for (MarginImage *mi in ((MarginContainer *)self.textContainer).marginImages) {
-//                if (mi.pos == range.location) {
-//                    NSLog(@"Margin image spotted!");
-//                    GlkTextBufferWindow *delegate = (GlkTextBufferWindow *)self.delegate;
-//                                    [delegate.glkctl speakString:mi.accessibilityRoleDescription];
-//                    break;
-//                }
-//            }
-//        }
-//        else if (self.string.length > range.location - 1 && [self.string characterAtIndex:range.location - 1] == NSAttachmentCharacter ) { //&& range.length < 2
-//            for (MarginImage *mi in ((MarginContainer *)self.textContainer).marginImages) {
-//                if (mi.pos == range.location - 1) {
-//                    NSLog(@"Margin image spotted!");
-//                    GlkTextBufferWindow *delegate = (GlkTextBufferWindow *)self.delegate;
-//                                   [delegate.glkctl speakString:mi.accessibilityRoleDescription];
-//                    break;
-//                }
-//            }
-//        }
-//    }
-//    [super setAccessibilitySelectedTextRange:range];
-//}
-
 - (NSArray *)accessibilityChildren {
     NSArray *children = [super accessibilityChildren];
 

application/GlkTextBufferWindow/MyAttachmentCell.h
@@ -7,6 +7,8 @@
 
 #import <Cocoa/Cocoa.h>
 
+@class MarginImage;
+
 NS_ASSUME_NONNULL_BEGIN
 
 // I suppose this is necessary to get rid of that ugly Markup menu on attached
@@ -15,6 +17,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface MyAttachmentCell : NSTextAttachmentCell <NSSecureCoding>
 
 @property (weak) NSAttributedString *attrstr;
+@property (weak) MarginImage *marginImage;
 @property NSUInteger pos;
 
 - (instancetype)initImageCell:(NSImage *)image

application/GlkTextBufferWindow/MyAttachmentCell.m
@@ -6,6 +6,7 @@
 //
 
 #import "MyAttachmentCell.h"
+#import "MarginImage.h"
 #include "glk.h"
 
 @interface MyAttachmentCell () <NSSecureCoding> {
@@ -85,22 +86,112 @@
 }
 
 - (NSString *)customA11yLabel {
+    if (_marginImage) {
+        return _marginImage.customA11yLabel;
+    }
+    
     NSString *label = self.image.accessibilityDescription;
     NSUInteger lastCharPos = _pos - 1;
 
     if (lastCharPos > _attrstr.length)
         lastCharPos = 0;
 
-   NSNumber *linkVal = (NSNumber *)[_attrstr attribute:NSLinkAttributeName
-                               atIndex:lastCharPos
-                        effectiveRange:nil];
+    NSNumber *linkVal = (NSNumber *)[_attrstr
+                                     attribute:NSLinkAttributeName
+                                     atIndex:lastCharPos
+                                     effectiveRange:nil];
 
     NSUInteger linkid = linkVal.unsignedIntegerValue;
 
     if (!label.length) {
-        label = [NSString stringWithFormat: @"%@attached image", linkid ? @"Clickable " : @""];
+        if (linkid)
+            label = @"Clickable attached image";
+        else
+            label = @"Attached image";
     }
     return label;
+}
+
+- (NSSize)cellSize {
+    if (align == imagealign_MarginLeft || align == imagealign_MarginRight) {
+        return NSZeroSize;
+    }
+    return [super cellSize];
+}
+
+- (NSRect)cellFrameForTextContainer:(NSTextContainer *)textContainer
+               proposedLineFragment:(NSRect)lineFrag
+                      glyphPosition:(NSPoint)position
+                     characterIndex:(NSUInteger)charIndex {
+    if (align == imagealign_MarginLeft || align == imagealign_MarginRight) {
+        return NSZeroRect;
+    }
+    return [super cellFrameForTextContainer:textContainer
+                       proposedLineFragment:lineFrag
+                              glyphPosition:position
+                             characterIndex:charIndex];
+}
+
+- (void)drawWithFrame:(NSRect)cellFrame
+               inView:(NSView *)controlView {
+    switch (align) {
+        case imagealign_MarginLeft:
+        case imagealign_MarginRight:
+            break;
+        default:
+            [super drawWithFrame:cellFrame
+                          inView:controlView];
+            break;
+    }
+}
+
+- (void)drawWithFrame:(NSRect)cellFrame
+               inView:(NSView *)controlView
+       characterIndex:(NSUInteger)charIndex {
+    switch (align) {
+        case imagealign_MarginLeft:
+        case imagealign_MarginRight:
+            break;
+        default:
+            [super drawWithFrame:cellFrame
+                          inView:controlView
+                  characterIndex:charIndex];
+            break;
+    }
+
+}
+
+- (void)drawWithFrame:(NSRect)cellFrame
+               inView:(NSView *)controlView
+       characterIndex:(NSUInteger)charIndex
+        layoutManager:(NSLayoutManager *)layoutManager  {
+    switch (align) {
+        case imagealign_MarginLeft:
+        case imagealign_MarginRight:
+            break;
+        default:
+            [super drawWithFrame:cellFrame
+                          inView:controlView
+                  characterIndex:charIndex
+                   layoutManager:layoutManager];
+            break;
+    }
+
+}
+
+- (void)highlight:(BOOL)flag
+        withFrame:(NSRect)cellFrame
+           inView:(NSView *)controlView {
+    switch (align) {
+        case imagealign_MarginLeft:
+        case imagealign_MarginRight:
+            break;
+        default:
+            [super highlight:flag
+                   withFrame:cellFrame
+                      inView:controlView];
+            break;
+    }
 }
 
 

commit a6a5f034410822529ccd7470efacb6a0dc34a8bb

Fix left margin image y position
I hope. It was broken by fedbea7 and apparent in Sensory Jam.
Also adjust margin image Y position for paragraphSpacingBefore
setting.

application/GlkTextBufferWindow/GlkTextBufferWindow.m
@@ -203,7 +203,7 @@ fprintf(stderr, "%s\n",                                                    \
         if (!NSEqualRects(self.pendingFrame, self.frame)) {
 
             if ([container hasMarginImages])
-                [container invalidateLayout];
+                [container invalidateLayout:nil];
 
             if (NSMaxX(self.pendingFrame) > NSWidth(self.glkctl.contentView.bounds) && NSWidth(self.pendingFrame) > 10) {
                 self.pendingFrame = NSMakeRect(self.pendingFrame.origin.x, self.pendingFrame.origin.y, NSWidth(self.glkctl.contentView.bounds) - self.pendingFrame.origin.x, self.pendingFrame.size.height);
@@ -715,7 +715,8 @@ fprintf(stderr, "%s\n",                                                    \
             [self showInsertionPoint];
             lastLineheight = self.theme.bufferNormal.font.boundingRectForFont.size.height;
             [self recalcBackground];
-            [container invalidateLayout];
+            if ([container hasMarginImages])
+                [container performSelector:@selector(invalidateLayout:) withObject:nil afterDelay:0.2];
         }
         if (!_pendingScrollRestore) {
             _pendingScrollRestore = YES;
@@ -764,7 +765,7 @@ fprintf(stderr, "%s\n",                                                    \
         bgnd = currentZColor.bg;
 
     [self recalcBackground];
-    [container invalidateLayout];
+    [container invalidateLayout:nil];
     _pendingClear = NO;
 }
 
@@ -1506,7 +1507,7 @@ replacementString:(id)repl {
     [[MyAttachmentCell alloc] initImageCell:image
                                andAlignment:align
                                   andAttStr:textstorage
-                                         at:textstorage.length - 1];
+                                         at:textstorage.length];
 
     if (align == imagealign_MarginLeft || align == imagealign_MarginRight) {
         if (_lastchar != '\n' && textstorage.length) {
@@ -1514,7 +1515,7 @@ replacementString:(id)repl {
             return;
         } else {
             [container addImage:image align: align at:
-             textstorage.length - 1 linkid:(NSUInteger)self.currentHyperlink];
+             textstorage.length linkid:(NSUInteger)self.currentHyperlink];
             cell.marginImage = container.marginImages.lastObject;
         }
     }
@@ -1529,7 +1530,7 @@ replacementString:(id)repl {
         [textstorage addAttribute:NSLinkAttributeName value:@(self.currentHyperlink) range:NSMakeRange(textstorage.length - 1, 1)];
     }
 
-    [textstorage addAttributes:styles[style] range:NSMakeRange(textstorage.length -1, 1)];
+    [textstorage addAttributes:styles[style] range:NSMakeRange(textstorage.length - 1, 1)];
 }
 
 - (void)flowBreak {

application/GlkTextBufferWindow/MarginContainer.h
@@ -27,7 +27,7 @@ NS_ASSUME_NONNULL_BEGIN
               at:(NSUInteger)top
           linkid:(NSUInteger)linkid;
 - (void)drawRect:(NSRect)rect;
-- (void)invalidateLayout;
+- (void)invalidateLayout:(nullable id)sender;
 - (void)unoverlap:(MarginImage *)image;
 - (BOOL)hasMarginImages;
 - (NSMutableAttributedString *)marginsToAttachmentsInString:

application/GlkTextBufferWindow/MarginContainer.m
@@ -62,7 +62,7 @@
     [self.layoutManager textContainerChangedGeometry:self];
 }
 
-- (void)invalidateLayout {
+- (void)invalidateLayout:(id)sender {
     for (MarginImage *i in _marginImages)
         [i uncacheBounds];
 

application/GlkTextBufferWindow/MarginImage.m
@@ -93,6 +93,9 @@
         theline = [layout lineFragmentRectForGlyphAtIndex:ourglyph.location
                                            effectiveRange:nil];
 
+        NSParagraphStyle *para = [textview.textStorage attribute:NSParagraphStyleAttributeName atIndex:_pos effectiveRange:nil];
+        theline.origin.y += para.paragraphSpacingBefore;
+
         /* set bounds to be at the same line as anchor but in left/right margin
          */
         if (_alignment == imagealign_MarginRight) {

application/RotorHandler.m
@@ -352,7 +352,7 @@
                 NSTextStorage *txtstorage = view.textStorage;
                 NSTextAttachment *attachment = [txtstorage attribute:NSAttachmentAttributeName atIndex:((NSValue *)child).rangeValue.location effectiveRange:nil];
                 MyAttachmentCell *cell = (MyAttachmentCell *)attachment.attachmentCell;
-                if (pos == cell.pos + 1) {
+                if (pos == cell.pos) {
                     currentItemIndex = index;
                     break;
                 }