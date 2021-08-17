7077d5ef5c82554f4b3491287ed7d8fe72646a6a
angstsmurf <ignalina@me.com>
2021-08-17 02:25:41 +0200

    Comment out this check for now

--- application/GlkTextBufferWindow/GlkTextBufferWindow.m
+++ application/GlkTextBufferWindow/GlkTextBufferWindow.m
@@ -832,10 +832,10 @@ fprintf(stderr, "%s\n",                                                    \
         NSLog(@"Null string!");
         return;
     }
-    if ([str characterAtIndex:(str.length - 1)] == '\0')
-        NSLog(@"Null terminated string!");
-    if ([str characterAtIndex:0] == '\0')
-        NSLog(@"Null prefixed string!");
+//    if ([str characterAtIndex:(str.length - 1)] == '\0')
+//        NSLog(@"Null terminated string!");
+//    if ([str characterAtIndex:0] == '\0')
+//        NSLog(@"Null prefixed string!");
 
     if (bufferTextstorage.length > 50000)
         bufferTextstorage = [bufferTextstorage attributedSubstringFromRange:NSMakeRange(25000, bufferTextstorage.length - 25000)].mutableCopy;
