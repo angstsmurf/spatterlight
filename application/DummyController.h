fc4173888a2801e1f5141d4f1b6be84912348f33
angstsmurf <ignalina@me.com>
2021-10-03 19:14:28 +0200

    User-defined settings to sync version strings across targets

---
+++ application/DummyController.h
@@ -0,0 +1,16 @@
+//
+//  DummyController.h
+//  Spatterlight
+//
+//  Created by Administrator on 2021-10-03.
+//
+
+#import "GlkController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DummyController : GlkController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ application/DummyController.m
@@ -0,0 +1,22 @@
+//
+//  DummyController.m
+//  Spatterlight
+//
+//  Created by Administrator on 2021-10-03.
+//
+
+#import "DummyController.h"
+
+@interface DummyController ()
+
+@end
+
+@implementation DummyController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
