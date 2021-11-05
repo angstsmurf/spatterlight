f8dd8c3ece4e8eed56e23a56931346b82c0bb6a5
angstsmurf <ignalina@me.com>
2021-11-05 19:35:14 +0100

    Update Babel Hugo detection
    The previous version missed the IFID of one game (World Builder).
    
    Also converts the output to uppercase, as the game PAXLess has it in
    lowercase in its dictionary.

--- babel/hugo.c
+++ babel/hugo.c
@@ -25,38 +25,44 @@
 #define HUGO_DICTTABLE 0x15
 #define HUGO_SYNTABLE 0x17
 
-#define HUGO_TEXTBANK 0x29
-
 #include "treaty_builder.h"
 #include <ctype.h>
 #include <stdio.h>
+
+static int is_uppercase_hexadecimal(char c)
+{
+ if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F'))
+  return 1;
+ return 0;
+}
 
 static int number_of_hexadecimals_before_hyphen(char *s, size_t len)
 {
  size_t offset = 0;
 
- while (offset < len && isxdigit(s[offset]))
+ while (offset < len && is_uppercase_hexadecimal(s[offset]))
   offset++;
 
- if (offset == len || (offset < len && s[offset] == '-'))
+ if (offset == len || s[offset] == '-')
   return offset;
 
  return 0;
 }
 
 /* We look for the pattern XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX (8-4-4-4-12)
- where X is a number or A-F (one Hugo game, PAXLess, uses lowercase letters, the others are all uppercase) */
+ * where X is a number or A-F
+ */
 static int isUUID(char *s)
 {
  if (!(number_of_hexadecimals_before_hyphen(s, 9) == 8))
   return 0;
- if (!(number_of_hexadecimals_before_hyphen(s+9, 5) == 4))
+ if (!(number_of_hexadecimals_before_hyphen(s + 9, 5) == 4))
   return 0;
- if (!(number_of_hexadecimals_before_hyphen(s+14, 5) == 4))
+ if (!(number_of_hexadecimals_before_hyphen(s + 14, 5) == 4))
   return 0;
- if (!(number_of_hexadecimals_before_hyphen(s+19, 5) == 4))
+ if (!(number_of_hexadecimals_before_hyphen(s + 19, 5) == 4))
   return 0;
- if (!(number_of_hexadecimals_before_hyphen(s+24, 12) == 12))
+ if (!(number_of_hexadecimals_before_hyphen(s + 24, 12) == 12))
   return 0;
  return 1;
 }
@@ -72,48 +78,49 @@ static char hugo_decode(char c)
 
 static uint32 read_hugo_addx(unsigned char *from)
 {
- return ((uint32) from[0])| ((uint32)from[1] << 8);
+ return ((uint32)from[0]) | ((uint32)from[1] << 8);
 }
 
-static int32 get_story_file_IFID(void *s_file, int32 extent, char *output, int32 output_extent)
+static int32 get_story_file_IFID(void *s_file, int32 extent, char *output,
+                                 int32 output_extent)
 {
- size_t i,j;
+ size_t i, j;
  char ser[9];
  char buffer[32];
  char UUID_candidate[37];
- char hyphen = '-'+20;
+ char hyphen = '-' + 20;
 
- char *story_file = (char *) s_file;
+ char *story_file = (char *)s_file;
 
- if (extent<0x0B) return INVALID_STORY_FILE_RV;
+ if (extent < HUGO_CODESTART)
+  return INVALID_STORY_FILE_RV;
 
- long dicttable  = read_hugo_addx((unsigned char *)story_file+HUGO_DICTTABLE)*16;
- long textbank = read_hugo_addx((unsigned char *)story_file+HUGO_TEXTBANK)*16;
+ long dicttable = read_hugo_addx((unsigned char *)story_file + HUGO_DICTTABLE) * 16;
 
- /* Older Hugo versions do not provide a textbank offset in the header */
- if (textbank < dicttable)
-  textbank = extent;
-
- for(i=dicttable+8;i<textbank-28;i++)
+ for (i = dicttable + 8; i < extent - 28; i++)
  {
-  /* First we look for an obfuscated hyphen, '-' + 20 */
-  /* We need to look 8 characters behind and 28 ahead */
-  if (story_file[i] == hyphen && i >= 8 && extent-i >= 28 &&
-      story_file[i+5] == hyphen &&
-      story_file[i+10] == hyphen &&
-      story_file[i+15] == hyphen)
+  /* First we look for an obfuscated hyphen, '-' + 20
+   * We need to look 8 characters behind and 28 ahead
+   */
+  if (story_file[i] == hyphen &&
+      i >= 8 && extent - i >= 28 &&
+      story_file[i + 5] == hyphen &&
+      story_file[i + 10] == hyphen &&
+      story_file[i + 15] == hyphen)
   {
-   for (j=0;j<36;j++)
+   for (j = 0; j < 36; j++)
    {
-    UUID_candidate[j] = hugo_decode(story_file[i+j-8]);
+    /* One Hugo game, PAXLess, uses lowercase letters in its IFID.
+     * We make it uppercase here.
+     */
+    UUID_candidate[j] = toupper(hugo_decode(story_file[i + j - 8]));
    }
    if (isUUID(UUID_candidate))
    {
     /* Found valid UUID at file offset i - 8 */
-    ASSERT_OUTPUT_SIZE((signed) 37);
-    memcpy(output,UUID_candidate,36);
-    output[36]='\0';
-    fprintf(stderr, "Found UUID at %lu\n", i-8);
+    ASSERT_OUTPUT_SIZE((signed)37);
+    memcpy(output, UUID_candidate, 36);
+    output[36] = '\0';
     return 1;
    }
   }
@@ -121,67 +128,68 @@ static int32 get_story_file_IFID(void *s_file, int32 extent, char *output, int32
 
  /* Found no UUID in file. Construct legacy IFID */
 
- memcpy(ser, (char *) story_file+0x03, 8);
- ser[8]=0;
+ memcpy(ser, (char *)story_file + 0x03, 8);
+ ser[8] = 0;
 
- for(j=0;j<8;j++)
-  if (!isalnum(ser[j])) ser[j]='-';
+ for (j = 0; j < 8; j++)
+  if (!isalnum(ser[j]))
+   ser[j] = '-';
 
- sprintf(buffer,"HUGO-%d-%02X-%02X-%s",story_file[0],story_file[1], story_file[2],ser);
+ sprintf(buffer, "HUGO-%d-%02X-%02X-%s", story_file[0], story_file[1],
+         story_file[2], ser);
 
- ASSERT_OUTPUT_SIZE((signed) strlen(buffer)+1);
- strcpy((char *)output,buffer);
+ ASSERT_OUTPUT_SIZE((signed)strlen(buffer) + 1);
+ strcpy((char *)output, buffer);
  return 1;
 }
 
-
-
 static int32 claim_story_file(void *story_file, int32 exten)
 {
- unsigned char *sf=(unsigned char *)story_file;
+ unsigned char *sf = (unsigned char *)story_file;
  int32 i;
  int32 scale;
- uint32 extent=(uint32) exten;
+ uint32 extent = (uint32)exten;
 
  if (!story_file || extent < 0x28) return  INVALID_STORY_FILE_RV;
 
- // /* 39 is the largest version currently accepted by the Hugo interpreter:
- //    https://github.com/garglk/garglk/blob/master/terps/hugo/source/hemisc.c#L1310-L1362
- // */
+ /* 39 is the largest version currently accepted by the Hugo interpreter:
+  * https://github.com/garglk/garglk/blob/master/terps/hugo/source/hemisc.c#L1310-L1362
+  */
  if (sf[0] > 39) return INVALID_STORY_FILE_RV;
 
- if (sf[0]<34) scale=4;
- else scale=16;
- for(i=3;i<HUGO_CODESTART;i++) if (sf[i]<0x20 || sf[i]>0x7e) return INVALID_STORY_FILE_RV;
- for(i=0x0b;i<0x18;i+=2)
-  if (read_hugo_addx(sf+i) * scale > extent) return INVALID_STORY_FILE_RV;
+ if (sf[0] < 34) scale = 4;
+ else scale = 16;
+ for (i = 3; i < HUGO_CODESTART; i++) if (sf[i] < 0x20 || sf[i] > 0x7e)
+  return INVALID_STORY_FILE_RV;
+ for (i = HUGO_CODESTART; i < 0x18; i += 2)
+  if (read_hugo_addx(sf + i) * scale > extent) return INVALID_STORY_FILE_RV;
 
- long objtable = read_hugo_addx(sf+HUGO_OBJTABLE)*scale;
- long proptable = read_hugo_addx(sf+HUGO_PROPTABLE)*scale;
+ long objtable = read_hugo_addx(sf + HUGO_OBJTABLE) * scale;
+ long proptable = read_hugo_addx(sf + HUGO_PROPTABLE) * scale;
  if (proptable <= objtable)
  {
   /* proptable is not after objtable, not a valid Hugo file */
   return INVALID_STORY_FILE_RV;
  }
- long eventtable = read_hugo_addx(sf+HUGO_EVENTTABLE)*scale;
+ long eventtable = read_hugo_addx(sf + HUGO_EVENTTABLE) * scale;
  if (eventtable <= proptable)
  {
   /* eventtable is not after proptable, not a valid Hugo file */
   return INVALID_STORY_FILE_RV;
  }
- long arraytable = read_hugo_addx(sf+HUGO_ARRAYTABLE)*scale;
+ long arraytable = read_hugo_addx(sf + HUGO_ARRAYTABLE) * scale;
  if (arraytable <= eventtable)
  {
   /* arraytable is not after eventtable, not a valid Hugo file */
   return INVALID_STORY_FILE_RV;
  }
- long syntable = read_hugo_addx(sf+HUGO_SYNTABLE)*scale;
+ long syntable = read_hugo_addx(sf + HUGO_SYNTABLE) * scale;
  if (syntable <= arraytable)
  {
   /* syntable is not after arraytable, not a valid Hugo file */
   return INVALID_STORY_FILE_RV;
  }
- long dicttable  = read_hugo_addx(sf+HUGO_DICTTABLE)*scale;
+ long dicttable  = read_hugo_addx(sf + HUGO_DICTTABLE) * scale;
  if (dicttable <= syntable)
  {
   /* dicttable is not after syntable, not a valid Hugo file */
