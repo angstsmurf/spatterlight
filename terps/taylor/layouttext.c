cff760566aec720b0875bd839f268c94d3816707
angstsmurf <ignalina@me.com>
2022-03-18 12:17:20 +0100

    layouttext: Prune initial spaces and periods

--- terps/taylor/layouttext.c
+++ terps/taylor/layouttext.c
@@ -41,7 +41,7 @@ char *LineBreakText(char *source, int columns, int *rows, int *length)
     int destpos = 0;
     int diff = 0;
     *rows = 0;
-    if (source[0] == ' ')
+    while (source[sourcepos] == ' ' || source[sourcepos] == '.')
         sourcepos++;
     while (source[sourcepos] != '\0') {
         while (col < columns && source[sourcepos] != '\0') {
