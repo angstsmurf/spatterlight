6b2354462286ee1e74a7a3efc33803839d2a6900
angstsmurf <ignalina@me.com>
2022-03-23 13:28:56 +0100

    TAYLOR: Add decompressz80.c
    Does not work with the 128 K Blizzard Pass z80 file yet.

---
+++ terps/taylor/decompressz80.h
@@ -0,0 +1,15 @@
+//
+//  decompressz80.h
+//  Spatterlight
+//
+//  Created by Administrator on 2022-01-24.
+//
+
+#ifndef decompressz80_h
+#define decompressz80_h
+
+/* Will return NULL on error or 0xc000 (49152) bytes of uncompressed raw data on
+ * success */
+uint8_t *DecompressZ80(uint8_t *raw_data, size_t *length);
+
+#endif /* decompressz80_h */
