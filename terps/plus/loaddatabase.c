79294ce67e1e69a840125ac07459c61595bf8276
angstsmurf <ignalina@me.com>
2022-09-10 16:09:32 +0200

    PLUS: Enlarge this array to be sure
    File paths can get very long.

--- terps/plus/loaddatabase.c
+++ terps/plus/loaddatabase.c
@@ -777,7 +777,7 @@ static int SetGame(const char *id_string, size_t length) {
 
 int FindAndAddImageFile(char *shortname, struct imgrec *rec) {
     int result = 0;
-    char filename[2048];
+    char filename[1024];
     int n = sprintf(filename, "%s%s.PAK", DirPath, shortname);
     if (n > 0) {
         FILE *infile=fopen(filename,"rb");
