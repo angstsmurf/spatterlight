a7678ab983363cc5f4cd37551e63428cb73dd66b
angstsmurf <ignalina@me.com>
2023-01-10 05:59:51 +0100

    BOCFEL: Update to 2.1

--- terps/bocfel/util.h
+++ terps/bocfel/util.h
@@ -21,37 +21,13 @@
 #define zprintflike(f, a)
 #endif
 
-// Values are usually stored in a uint16_t because most parts of the
-// Z-machine make use of 16-bit unsigned integers. However, in a few
-// places the unsigned value must be treated as signed. The “obvious”
-// solution is to simply convert to an int16_t, but this is technically
-// implementation-defined behavior in C++ and not guaranteed to provide
-// the expected result. In order to be maximally portable, this
-// function converts a uint16_t to its int16_t counterpart manually.
-// Although it ought to be slower, both Gcc and Clang recognize this
-// construct and generate the same code as a direct conversion. An
-// alternative direct conversion method is included here for reference.
-#if 1
-static inline int16_t as_signed(uint16_t n)
-{
-    return (n & 0x8000) ? static_cast<long>(n) - 0x10000L : n;
-}
-#else
-static inline int16_t as_signed(uint16_t n)
-{
-    return n;
-}
-#endif
+int16_t as_signed(uint16_t n);
 
 #ifndef ZTERP_NO_SAFETY_CHECKS
 [[noreturn]]
 zprintflike(1, 2)
 void assert_fail(const char *fmt, ...);
-#ifdef SPATTERLIGHT
-#define ZASSERT(expr, ...) if(!(expr)) assert_fail(__VA_ARGS__);
-#else
 #define ZASSERT(expr, ...)	do { if (!(expr)) assert_fail(__VA_ARGS__); } while (false)
-#endif
 #else
 #define ZASSERT(expr, ...)	((void)0)
 #endif
@@ -78,6 +54,5 @@ zprintflike(1, 2)
 std::string fstring(const char *fmt, ...);
 std::string ltrim(const std::string &s);
 std::string rtrim(const std::string &s);
-std::string operator"" _s(const char *s, size_t len);
 
 #endif
