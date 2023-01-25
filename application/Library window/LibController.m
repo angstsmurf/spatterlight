31cca12cdfcad24ca61d5eab548d2f2c6a714d8f
angstsmurf <ignalina@me.com>
2023-01-25 08:23:37 +0100

    LibController: Delay restored search a bit

--- application/Library window/LibController.m
+++ application/Library window/LibController.m
@@ -264,7 +264,9 @@ fprintf(stderr, "%s\n",                                                    \
     NSString *searchText = [state decodeObjectOfClass:[NSString class] forKey:@"searchText"];
     if (searchText.length) {
         _searchField.stringValue = searchText;
-        [self.tableViewController searchForGames:_searchField];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^(void) {
+            [self.tableViewController searchForGames:self.searchField];
+        });
     }
     NSArray *selectedIfids = [state decodeObjectOfClass:[NSArray class] forKey:@"selectedGames"];
     [self.tableViewController updateTableViews];
