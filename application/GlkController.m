c9afdd6c5bbfa0e8c1108437f81f6812569dde76
angstsmurf <ignalina@me.com>
2024-11-12 16:47:54 +0100

    GlkController: Add VoiceOver delay to speakOnBecomingKey
    Somewhat arbitrary, but it seems to help. Placebo?

--- application/GlkController.m
+++ application/GlkController.m
@@ -4927,7 +4927,9 @@ startCustomAnimationToEnterFullScreenWithDuration:(NSTimeInterval)duration {
         _shouldCheckForMenu = YES;
         [self checkZMenuAndSpeak:NO];
         if (_theme.vODelayOn && !_mustBeQuiet) {
-            [self speakMostRecentAfterDelay];
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [self speakMostRecentAfterDelay];
+            });
         }
     } else {
         _zmenu = nil;
