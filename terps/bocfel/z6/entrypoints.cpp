edafd4a306805edf2176517e4f3511e12850983a
angstsmurf <ignalina@me.com>
2025-04-11 08:22:44 +0200

    Bocfel: entrypoints.c: Silence debug fprintfs

--- terps/bocfel/z6/entrypoints.cpp
+++ terps/bocfel/z6/entrypoints.cpp
@@ -1681,22 +1681,22 @@ void find_entrypoints(void) {
 
     for (auto &entrypoint : entrypoints) {
         if (is_game(entrypoint.game)) {
-            fprintf(stderr, "Looking for entrypoint %s (starting at 0x%x)\n", entrypoint.title.c_str(), start);
+//            fprintf(stderr, "Looking for entrypoint %s (starting at 0x%x)\n", entrypoint.title.c_str(), start);
             if (entrypoint.pattern.size()) {
                 int32_t offset = find_pattern_in_mem(entrypoint.pattern, start, end - start);
                 if (offset != -1) {
                     entrypoint.found_at_address = offset + entrypoint.offset;
                     start = entrypoint.found_at_address;
-                    fprintf(stderr, "Found routine %s at offset 0x%04x\n", entrypoint.title.c_str(), start);
+//                    fprintf(stderr, "Found routine %s at offset 0x%04x\n", entrypoint.title.c_str(), start);
                     if (entrypoint.stub_original) {
                         // Overwrite original byte with rtrue;
                         store_byte(entrypoint.found_at_address, 0xb0);
                     }
-                } else {
-                    fprintf(stderr, "Did not find it!\n");
+//                } else {
+//                    fprintf(stderr, "Did not find it!\n");
                 }
-            } else {
-                fprintf(stderr, "Did not find it! (No pattern)\n");
+//            } else {
+//                fprintf(stderr, "Did not find it! (No pattern)\n");
             }
         }
     }
