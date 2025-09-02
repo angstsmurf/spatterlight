2ef136fbe63ab671c9fc57c596f23542e4f40c3b
angstsmurf <ignalina@me.com>
2025-09-02 22:42:44 +0200

    Guard against empty destination file URL path
    When draggin and dropping images. I think this
    happens when dragging between Spatterlight views.

--- application/Library window/ImageView.m
+++ application/Library window/ImageView.m
@@ -979,7 +979,8 @@
         }
 
         // And finally, tell the receiver where we wrote our file
-        [sender setString:destinationFileURL.path forType:PasteboardFileURLPromise];
+        if (destinationFileURL.path.length > 0)
+            [sender setString:destinationFileURL.path forType:PasteboardFileURLPromise];
     }
 }
 
