0a2dff1056337d4ab39a77c7626657e3644e843b
angstsmurf <ignalina@me.com>
2025-09-30 16:07:41 +0200

    glkimp cgunicode.c: Don't apply the same binary & operand twice

--- glkimp/cgunicod.c
+++ glkimp/cgunicod.c
@@ -122,9 +122,9 @@ glui32 gli_parse_utf8(unsigned char *buf, glui32 buflen,
                 gli_strict_warning("malformed three-byte character");
                 break;
             }
-            res = (((val0 & 0xf)<<12)  & 0x0000f000);
+            res =  (((val0 & 0xf)<<12) & 0x0000f000);
             res |= (((val1 & 0x3f)<<6) & 0x00000fc0);
-            res |= (((val2 & 0x3f))    & 0x0000003f);
+            res |= (  val2 & 0x3f                  );
             out[outpos++] = res;
             continue;
         }
@@ -156,7 +156,7 @@ glui32 gli_parse_utf8(unsigned char *buf, glui32 buflen,
             res = (((val0 & 0x7)<<18)   & 0x1c0000);
             res |= (((val1 & 0x3f)<<12) & 0x03f000);
             res |= (((val2 & 0x3f)<<6)  & 0x000fc0);
-            res |= (((val3 & 0x3f))     & 0x00003f);
+            res |= (  val3 & 0x3f                 );
             out[outpos++] = res;
             continue;
         }
