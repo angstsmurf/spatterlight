7462ff65766d2d5f2eeb4a83fcdb79af2ae02419
angstsmurf <ignalina@me.com>
2026-01-14 06:00:24 +0100

    SplitView: Increase title column maximum width
    To fit those long Atari Scott Adams disk image file names.
    
    Updated SplitView to remove constraint warnings
    
    Not yet tested on older systems.

---
+++ terps/scott/saga/atari_8bit_vector_draw.c
@@ -0,0 +1,8 @@
+//
+//  atari_8bit_vector_draw.c
+//  scott
+//
+//  Created by Administrator on 2026-01-13.
+//
+
+#include "atari_8bit_vector_draw.h"
---
+++ terps/scott/saga/atari_8bit_vector_draw.h
@@ -0,0 +1,13 @@
+//
+//  atari_8bit_vector_draw.h
+//  scott
+//
+//  Created by Administrator on 2026-01-13.
+//
+
+#ifndef atari_8bit_vector_draw_h
+#define atari_8bit_vector_draw_h
+
+#include <stdio.h>
+
+#endif /* atari_8bit_vector_draw_h */
