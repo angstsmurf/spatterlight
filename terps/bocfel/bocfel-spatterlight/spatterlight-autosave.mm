1f80d7b9fb656631808ee63623213a965aecbf72
angstsmurf <ignalina@me.com>
2026-05-21 22:11:36 +0200

    BOCFEL: Show optional rebus message
    If VoiceOver is active, nudge the player towards
    look up the solution to the rebus in the built-in
    InvisiClues. Also auto-save the flag about having
    shown this message alreay.

--- terps/bocfel/bocfel-spatterlight/spatterlight-autosave.mm
+++ terps/bocfel/bocfel-spatterlight/spatterlight-autosave.mm
@@ -268,6 +268,7 @@ static void spatterlight_library_archive(TempLibrary *library, NSCoder *encoder)
     [encoder encodeInt32:library_state.errorwintag forKey:@"bocfel_errorwintag"];
     [encoder encodeInt32:library_state.graphicswintag forKey:@"bocfel_graphicswintag"];
     [encoder encodeInt32:library_state.z0_right_status_tag forKey:@"bocfel_z0_right_status_tag"];
+    [encoder encodeBool:library_state.z0_shown_rebus_hint_message forKey:@"bocfel_z0_shown_rebus_hint_message"];
     [encoder encodeInt32:library_state.blorbfiletag forKey:@"bocfel_blorbfiletag"];
     [encoder encodeInt32:(int32_t)library_state.routine forKey:@"bocfel_routine"];
     [encoder encodeInt32:(int32_t)library_state.queued_sound forKey:@"bocfel_next_sample"];
@@ -334,6 +335,7 @@ static void spatterlight_library_unarchive(TempLibrary *library, NSCoder *decode
     library_state.errorwintag = [decoder decodeInt32ForKey:@"bocfel_errorwintag"];
     library_state.graphicswintag = [decoder decodeInt32ForKey:@"bocfel_graphicswintag"];
     library_state.z0_right_status_tag = [decoder decodeInt32ForKey:@"bocfel_z0_right_status_tag"];
+    library_state.z0_shown_rebus_hint_message = [decoder decodeBoolForKey:@"bocfel_z0_shown_rebus_hint_message"];
     library_state.blorbfiletag = [decoder decodeInt32ForKey:@"bocfel_blorbfiletag"];
     library_state.routine = [decoder decodeInt32ForKey:@"bocfel_routine"];
     library_state.queued_sound = [decoder decodeInt32ForKey:@"bocfel_next_sample"];
